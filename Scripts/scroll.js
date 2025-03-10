9c99fc8a309e88452bc7f470119a8fb19438ac2f
W1ll1am-c <123williamchen@gmail.com>
2025-03-10 12:39:14 -0400

    New Cardback

--- Scripts/scroll.js
+++ Scripts/scroll.js
@@ -1,15 +1,15 @@
 // Define card data
 const cards = [
-    { name: "Satyr", front: "image/satyr.PNG", back: "image/cb.png" },
-    { name: "Cerberus", front: "image/c.png", back: "image/cb.png" },
-    { name: "Theta", front: "image/th.png", back: "image/cb.png" },
-    { name: "Tau", front: "image/ta.png", back: "image/cb.png" },
-    { name: "Gorgon", front: "image/g.png", back: "image/cb.png" },
-    { name: "Banana Comeback", front: "image/bc.png", back: "images/cb.png" },
-    { name: "Potion Comeback", front: "image/pp.png", back: "image/cb.png" },
-    { name: "Meander", front: "image/m.PNG", back: "image/cb.png" },
-    { name: "Unlucky Comeback", front: "image/uc.png", back: "image/cb.png" },
-    { name: "Scale Comeback", front: "image/sc.png", back: "image/cb.png" }
+    { name: "Satyr", front: "image/satyr.PNG", back: "image/newcardback.png" },
+    { name: "Cerberus", front: "image/c.png", back: "image/newcardback.png" },
+    { name: "Theta", front: "image/th.png", back: "image/newcardback.png" },
+    { name: "Tau", front: "image/ta.png", back: "image/newcardback.png" },
+    { name: "Gorgon", front: "image/g.png", back: "image/newcardback.png" },
+    { name: "Banana Comeback", front: "image/bc.png", back: "image/newcardback.png" },
+    { name: "Potion Comeback", front: "image/pp.png", back: "image/newcardback.png" },
+    { name: "Meander", front: "image/m.PNG", back: "image/newcardback.png" },
+    { name: "Unlucky Comeback", front: "image/uc.png", back: "image/newcardback.png" },
+    { name: "Scale Comeback", front: "image/sc.png", back: "image/newcardback.png" }
 ];
 
 let currentIndex = 0;
